de91ddda0d88452a0353d25f2b5e6e0f2955eee2
allangd.dev <allangd.dev@gmail.com>
2024-03-08 16:09:49 +0000

    Criação do formulario de login e cadastro

--- lib/Views/autenticacao_tela.dart
+++ lib/Views/autenticacao_tela.dart
@@ -1,28 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/_common/minhas_cores.dart';
 
-class AutenticacaoTela extends StatelessWidget {
+class AutenticacaoTela extends StatefulWidget {
   const AutenticacaoTela({super.key});
+
+  @override
+  State<AutenticacaoTela> createState() => _AutenticacaoTelaState();
+}
+
+class _AutenticacaoTelaState extends State<AutenticacaoTela> {
+  bool queroEntrar = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter , colors: [MinhasCores.azulTopoGradiente, MinhasCores.azulBaixoGradiente,])),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset("assets/logo.png", height: 128,),
-              const Text("Flutter Gym",textAlign: TextAlign.center , style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),)
-            ],
-          ),
-        ],
-      )
-    );
+        backgroundColor: Colors.blue,
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                    MinhasCores.azulTopoGradiente,
+                    MinhasCores.azulBaixoGradiente,
+                  ])),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset(
+                        "assets/logo.png",
+                        height: 128,
+                      ),
+                      const Text(
+                        "Flutter Gym",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 48,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(label: Text("E-mail")),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(label: Text("Senha")),
+                        obscureText: true,
+                      ),
+                      Visibility(visible: !queroEntrar, child: Column(children: [
+                        TextFormField(
+                        decoration: const InputDecoration(label: Text("Confirme senha")),
+                        obscureText: true,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(label: Text("Nome")),
+                      ),
+                      ],)),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {}, child: Text((queroEntrar)?"Entrar":"Cadastrar")),
+                      const SizedBox(height: 16,),
+                      TextButton(
+                          onPressed: () {
+                            setState(() {
+                              queroEntrar = !queroEntrar;
+                            });
+                          },
+                          child: Text((queroEntrar)?"Não tem cota? Cadastre-se": "Já tem uma conta? Entre!")),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
--- lib/_common/minhas_cores.dart
+++ lib/_common/minhas_cores.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MinhasCores {
   static const Color azulPrimario = Color(0xFF1984DB);
 
-  static const Color azulTopoGradiente = Color.fromARGB(255, 79, 191, 240);
-  static const Color azulBaixoGradiente = Color(0xFF0078D4);
+  static const Color azulBaixoGradiente = Color(0xFF4FBFF0);
+  static const Color azulTopoGradiente = Color(0xFF0078D4);
 
 }
