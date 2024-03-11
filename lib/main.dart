25eff3d5ca11b74455bd98f7b3706e1709feb589
AllanGd <allan_gd@hotmail.com>
2024-03-11 23:48:11 +0000

    ColorScheme alterado

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
       home: const AutenticacaoTela(),
